7cdd832bc14a9f8f9a8f744b6f4ee880e9959c5b
Shaqqour M <mshaqqourm@gmail.com>
2019-12-03 04:45:09 +0000

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,25 @@
 class User < ActiveRecord::Base
+    has_secure_password
+    validates :username, presence: true
+    after_initialize :init
+
+    def init
+      self.balance ||= 0.0
+    end
+
+    def deposit(amount)
+      self.balance += amount
+      self.save
+    end
+
+    def withdraw(amount)
+      if self.balance >= amount
+        self.balance -= amount
+        self.save
+        return true
+      else
+        return false
+      end
+    end
+
 end
